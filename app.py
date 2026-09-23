79f0a48f7d40296917c83cb2404a9b6f141977b6
UshaRani <usha@gmail.com>
2026-09-23 15:13:36 +0530

    Test automatic Jenkins trigger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! Jenkins CI/CD Pipeline is working."
+    return "Hello! Jenkins CI/CD Pipeline is working automatically."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
